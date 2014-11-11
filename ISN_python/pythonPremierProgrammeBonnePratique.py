0eb973058b6bf730601334d0a0ca1f40043b6f3f
ym001 <yves.mercadier@gmail.com>
2014-11-11 11:30:37 +0100

    11/11/2014

---
+++ ISN_python/pythonPremierProgrammeBonnePratique.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  pythonPremierProgrammeBonnePratique.py
+#  
+#  Copyright 2014 yves Mercadier <yves.mercadier@ac-montpellier.fr>
+#  
+
+print "   ---> Debut du programme.\n"
+
+#Les entrees du programme
+prenom = raw_input("Quel est ton prenom? ")
+annee  = input("Quelle est ton annee de naissance? ")
+
+#le calcul
+age    = 2015-annee
+
+#les sorties du programme
+print "\nBonjour "+prenom+"."
+print "En 2015 tu auras ",age," ans."
+
+print "\n   ---> Fin du programme."
+
