b6978d8a3d45820c2323013bc66133c7d9b6af15
ym001 <yves.mercadier@gmail.com>
2014-12-05 14:37:39 +0100

    05/12/2014

--- ISN_python/pythonPremierTest.py
+++ ISN_python/pythonPremierTest.py
@@ -1,26 +1,53 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
-#  pythonPremierProgrammeBonnePratique.py
+#  pythonPremierTest.py
 #  
 #  Copyright 2014 yves Mercadier <yves.mercadier@ac-montpellier.fr>
 #  
 
-print "   ---> Debut du programme.\n"
 
 #Les entrees du programme
-prenom = raw_input("Quel est ton prenom? ")
-annee  = input("Quelle est ton annee de naissance? ")
+#specifications
+#parametre : 
+#	aucun
+#retour : 
+#	a un nombre entier representant l'age de la personne.
 
-#le calcul
-age    = 2015-annee
-if age>18:
-#les sorties du programme
-	print "\nBonjour "+prenom+"."
-	print "En 2015 tu pourras conduire une voiture :-)"
-else:
-	print "\nBonjour "+prenom+"."
-	print "En 2015 tu ne pourras pas conduire une voiture. Desole..."
+def entree():
+	#Les entrees du programme
+	print ("Aurez-vous l'age de conduire une voiture en 2015 ?\n")
+	a = int(input("Quelle est ton annee de naissance? "))
+	return a
 	
-print "\n   ---> Fin du programme."
+#Calcul de l'age de la personne'
+#specifications
+#parametre : 
+#	d un nombre entier representant la date de naissance
+#retour
+#	a un nombre entier represantant l'age de la personne 
+def metier(d):
+	#le calcul
+	a=2015-d  
+	return a
 
+#Affichage
+#specifications
+#parametre : 
+#	a un nombre entier represantant l'age de la personne 
+#retour
+#	aucun
+def pourImpression(v):
+	strImpression="";""
+	if age>18:
+		strImpression="\nEn 2015 tu pourras conduire une voiture :-)"
+	else:
+		strImpression="\nEn 2015 tu ne pourras pas conduire une voiture. Desole..."
+	return strImpression;
+
+
+print ("   ---> Debut du programme.\n")
+annee=entree()
+age=metier(annee)
+print(pourImpression(age))
+print ("\n   ---> Fin du programme.")
