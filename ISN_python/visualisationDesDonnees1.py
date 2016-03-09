0b3148cb74983139ffb64a54bee781c9e89f81cc
Yves Mercadier <yves.mercadier@gmail.com>
2016-03-09 06:01:44 +0100

    Create visualisationDesDonnees1.py

---
+++ ISN_python/visualisationDesDonnees1.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  graphique1.py
+#  
+#  Copyright 2016 yves <yves.mercadier@ac-montpellier.fr>
+
+from pylab import *
+
+x = array([1, 3, 4, 6])
+y = array([2, 3, 5, 1])
+
+plot(x, y)
+
+show() # affiche la figure a l'ecran
